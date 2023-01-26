e11b22a34a91e7b6daece0eb98206e735f5452a6
Angel Melendez <amangelmelendez@gmail.com>
2023-01-26 03:49:59 -0700

    Decoupled frontend and backend for deployment purposes

--- frontend/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/iquit/Desktop/_/Reagan_Corp.%20/Reagan/frontend/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- frontend/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "/Users/iquit/Desktop/_/Reagan_Corp. /Reagan/frontend/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
